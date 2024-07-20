67a67ac99bfc47d410709fad32d43250e7482f17
arh <a.hafizi9@outlook.com>
2024-07-20 17:31:01 +0330

    add dtos + pipes

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,36 @@
-export class CreateUserDto {}
+import {
+    IsEmail,
+    IsNotEmpty,
+    IsOptional,
+    IsString,
+    Matches,
+    MaxLength,
+    MinLength,
+} from 'class-validator';
+
+export class CreateUserDto {
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(3)
+    @MaxLength(96)
+    firstName: string;
+
+    @IsString()
+    @IsOptional()
+    @MinLength(3)
+    @MaxLength(96)
+    lastName?: string;
+
+    @IsEmail()
+    @IsNotEmpty()
+    email: string;
+
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(6)
+    @Matches(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/, {
+        message:
+            'Minimum six characters, at least one letter, one number and one special character',
+    })
+    password: string;
+}
