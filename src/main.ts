7ecf3344ce590479753b5ebacbe0bcead54ab195
arh <a.hafizi9@outlook.com>
2024-07-21 17:44:39 +0330

    setup swagger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
@@ -11,6 +12,20 @@ async function bootstrap() {
             transform: true,
         }),
     );
+
+    const config = new DocumentBuilder()
+        .setTitle('NestJs Blog API')
+        .setTermsOfService('http://localhost:3000/terms-of-service')
+        .setLicense(
+            'MIT License',
+            'https://github.com/git/git-scm.com/blob/main/MIT-LICENSE.txt',
+        )
+        .addServer('http://localhost:3000/')
+        .setVersion('1.0')
+        .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
     await app.listen(3000);
 }
 bootstrap();
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -10,8 +10,10 @@ import {
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { UsersService } from './users.service';
+import { ApiTags } from '@nestjs/swagger';
 
 @Controller('users')
+@ApiTags('Users')
 export class UsersController {
     constructor(private readonly usersService: UsersService) {}
 
