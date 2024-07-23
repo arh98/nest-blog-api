75a76f12735b306145644de30f50491532bbd606
arh <a.hafizi9@outlook.com>
2024-07-23 14:39:05 +0330

    register entities for autoloading

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from 'src/users/users.module';
 import { PostsController } from './posts.controller';
 import { PostsService } from './posts.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Post } from './entities/post.entity';
 
 @Module({
     controllers: [PostsController],
     providers: [PostsService],
-    imports: [UsersModule],
+    imports: [UsersModule, TypeOrmModule.forFeature([Post])],
 })
 export class PostsModule {}
