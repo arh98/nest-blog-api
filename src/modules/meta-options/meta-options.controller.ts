9e69da48df40f52bbbe6a6fb64d44d0f28c54186
arh <a.hafizi9@outlook.com>
2024-08-17 19:54:48 +0330

    remove unneeded methods

--- src/modules/meta-options/meta-options.controller.ts
+++ src/modules/meta-options/meta-options.controller.ts
@@ -1,42 +1,15 @@
-import {
-    Controller,
-    Get,
-    Post,
-    Body,
-    Patch,
-    Param,
-    Delete,
-} from '@nestjs/common';
-import { MetaOptionsService } from './meta-options.service';
+import { Body, Controller, Post } from '@nestjs/common';
 import { CreateMetaOptionDto } from './dto/create-meta-option.dto';
-import { UpdateMetaOptionDto } from './dto/update-meta-option.dto';
+import { MetaOptionsService } from './meta-options.service';
+import { ApiTags } from '@nestjs/swagger';
 
 @Controller('meta-options')
+@ApiTags('meta-options')
 export class MetaOptionsController {
     constructor(private readonly metaOptsService: MetaOptionsService) {}
 
     @Post()
     create(@Body() dto: CreateMetaOptionDto) {
         return this.metaOptsService.create(dto);
-    }
-
-    @Get()
-    findAll() {
-        return this.metaOptsService.findAll();
-    }
-
-    @Get(':id')
-    findOne(@Param('id') id: string) {
-        return this.metaOptsService.findOne(+id);
-    }
-
-    @Patch(':id')
-    update(@Param('id') id: string, @Body() dto: UpdateMetaOptionDto) {
-        return this.metaOptsService.update(+id, dto);
-    }
-
-    @Delete(':id')
-    remove(@Param('id') id: string) {
-        return this.metaOptsService.remove(+id);
     }
 }
--- src/modules/meta-options/meta-options.service.ts
+++ src/modules/meta-options/meta-options.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateMetaOptionDto } from './dto/create-meta-option.dto';
-import { UpdateMetaOptionDto } from './dto/update-meta-option.dto';
 import { MetaOption } from './entities/meta-option.entity';
 
 @Injectable()
@@ -15,21 +14,5 @@ export class MetaOptionsService {
     async create(dto: CreateMetaOptionDto) {
         const metaOption = this.metaOpsRepo.create(dto);
         return await this.metaOpsRepo.save(metaOption);
-    }
-
-    findAll() {
-        return `This action returns all metaOptions`;
-    }
-
-    findOne(id: number) {
-        return `This action returns a #${id} metaOption`;
-    }
-
-    update(id: number, dto: UpdateMetaOptionDto) {
-        return `This action updates a #${id} metaOption`;
-    }
-
-    remove(id: number) {
-        return `This action removes a #${id} metaOption`;
     }
 }
