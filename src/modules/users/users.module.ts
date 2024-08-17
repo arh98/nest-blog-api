5c37c5ace994935412489a224577fdb5683295d1
arh <a.hafizi9@outlook.com>
2024-08-17 19:40:21 +0330

    update imports

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,7 +1,7 @@
-import { forwardRef, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { UsersService } from './providers/users.service';
 import { UsersController } from './users.controller';
-import { AuthModule } from 'src/modules/auth/auth.module';
+// import { AuthModule } from 'src/modules/auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { CreateManyUsersService } from './providers/create-many-users.service';
@@ -9,7 +9,7 @@ import { CreateManyUsersService } from './providers/create-many-users.service';
 @Module({
     controllers: [UsersController],
     providers: [UsersService, CreateManyUsersService],
+    imports: [TypeOrmModule.forFeature([User])],
     exports: [UsersService],
-    imports: [TypeOrmModule.forFeature([User]), forwardRef(() => AuthModule)],
 })
 export class UsersModule {}
