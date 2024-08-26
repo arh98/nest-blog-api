e5ece3a2ee9926b2e3c20bb6b25b3a8bfa3631ca
arh <a.hafizi9@outlook.com>
2024-08-26 13:48:57 +0330

    set public routes

--- src/modules/tags/tags.controller.ts
+++ src/modules/tags/tags.controller.ts
@@ -4,34 +4,38 @@ import { CreateTagDto } from './dto/create-tag.dto';
 import { UpdateTagDto } from './dto/update-tag.dto';
 import { ApiTags } from '@nestjs/swagger';
 import { ParamId } from 'src/common/decorators/param-id.decorator';
+import { AuthType } from '../auth/authentication/enums/auth-type.enum';
+import { Auth } from '../auth/decorators/auth-type.decorator';
 
 @Controller('tags')
 @ApiTags('Tags')
 export class TagsController {
-    constructor(private readonly tagsService: TagsService) {}
+    constructor(private readonly service: TagsService) {}
 
     @Post()
     create(@Body() dto: CreateTagDto) {
-        return this.tagsService.create(dto);
+        return this.service.create(dto);
     }
 
     @Get()
+    @Auth(AuthType.None)
     findAll() {
-        return this.tagsService.findAll();
+        return this.service.findAll();
     }
 
     @Get(':id')
+    @Auth(AuthType.None)
     findOne(@ParamId() id: number) {
-        return this.tagsService.findOne(id);
+        return this.service.findOne(id);
     }
 
     @Patch(':id')
     update(@ParamId() id: number, @Body() dto: UpdateTagDto) {
-        return this.tagsService.update(id, dto);
+        return this.service.update(id, dto);
     }
 
     @Delete(':id')
     remove(@ParamId() id: number) {
-        return this.tagsService.remove(id);
+        return this.service.remove(id);
     }
 }
