e28a7f34b3097f81d0d49e342ac9ac283dfd54ba
arh <a.hafizi9@outlook.com>
2024-08-28 19:01:06 +0330

    remove unneeded userId

--- src/modules/me/dto/add-bookmark.dto.ts
+++ src/modules/me/dto/add-bookmark.dto.ts
@@ -3,9 +3,6 @@ import { BookmarkType } from '../enums/bookmark-type.enum';
 
 export class AddBookmarkDto {
     @IsNumber()
-    userId: number;
-
-    @IsNumber()
     postId: number;
 
     @IsEnum(BookmarkType)
