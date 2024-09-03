f1bd47834a0c4c72d2371fab764e7cf96c0cb80f
arh <a.hafizi9@outlook.com>
2024-09-03 17:48:49 +0330

    add role id to jwt payload

--- src/modules/auth/interfaces/active-user.interface.ts
+++ src/modules/auth/interfaces/active-user.interface.ts
@@ -1,13 +1,11 @@
-// import { Role } from 'src/users/enums/user-role.enum';
-// import { PermissionType } from '../authorization/permission.type';
 
 export interface IActiveUser {
-    // user id
     sub: number;
 
     email: string;
 
-    // role: Role;
-
-    // permissions: PermissionType[];
+    roleId: number;
+    
 }
+
+// permissions: PermissionType[];
